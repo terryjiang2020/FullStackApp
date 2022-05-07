58c1797c874aede39496deeb09212cf8e4bcd7cc
Administrator <sk@unelma.ee>
2022-05-07 09:00:50 +0300

    Fixed missing column in api

--- api/index.php
+++ api/index.php
@@ -8,9 +8,7 @@ include 'DBConnect.php';
 $objectDB = new DbConnect;
 $conn = $objectDB->connect();
 //var_dump($connection);
-
 //print_r($_POST);
-
 //print_r(file_get_contents('php://input'));
 
 $user = file_get_contents('php://input');
@@ -37,13 +35,15 @@ switch ($method) {
 
   case "POST":
     $user = json_decode(file_get_contents('php://input'));
-    $sql = "INSERT INTO users(id, name, email, mobile, created_at) VALUES(null, :name, :email, :mobile, :created_at)";
+    $sql = "INSERT INTO users(id, name, email, mobile, created_at, updated_at) VALUES(null, :name, :email, :mobile, :created_at, :updated_at)";
     $stmt = $conn->prepare($sql);
     $created_at = date('Y-m-d');
+    $updated_at = date('Y-m-d');
     $stmt->bindParam(':name', $user->name);
     $stmt->bindParam(':email', $user->email);
     $stmt->bindParam(':mobile', $user->mobile);
     $stmt->bindParam(':created_at', $created_at);
+    $stmt->bindParam(':updated_at', $updated_at);
 
     if ($stmt->execute()) {
       $response = ['status' => 1, 'message' => 'Record created successfully.'];
