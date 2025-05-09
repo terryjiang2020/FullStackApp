bb5e6f467d529bdec4d46bc725131da1160b58fa
TerryAtRocketVentures <68800619+terryjiang2020@users.noreply.github.com>
2025-05-10 04:12:49 +1200

    remove post user

--- api/index.php
+++ api/index.php
@@ -47,27 +47,27 @@ switch ($method) {
     echo json_encode($users);
     break;
 
-//   case "POST":
-//     print_r('POST is triggered');
-//     $user = json_decode(file_get_contents('php://input'));
-//     $sql = "INSERT INTO users(id, name, email, mobile, created_at, updated_at) VALUES(null, :name, :email, :mobile, :created_at, :updated_at)";
-//     $stmt = $conn->prepare($sql);
-//     $created_at = date('Y-m-d');
-//     $updated_at = date('Y-m-d');
-//     $stmt->bindParam(':name', $user->name);
-//     $stmt->bindParam(':email', $user->email);
-//     $stmt->bindParam(':mobile', $user->mobile);
-//     $stmt->bindParam(':created_at', $created_at);
-//     $stmt->bindParam(':updated_at', $updated_at);
+  case "POST":
+    print_r('POST is triggered');
+    $user = json_decode(file_get_contents('php://input'));
+    $sql = "INSERT INTO users(id, name, email, mobile, created_at, updated_at) VALUES(null, :name, :email, :mobile, :created_at, :updated_at)";
+    $stmt = $conn->prepare($sql);
+    $created_at = date('Y-m-d');
+    $updated_at = date('Y-m-d');
+    $stmt->bindParam(':name', $user->name);
+    $stmt->bindParam(':email', $user->email);
+    $stmt->bindParam(':mobile', $user->mobile);
+    $stmt->bindParam(':created_at', $created_at);
+    $stmt->bindParam(':updated_at', $updated_at);
 
-//     if ($stmt->execute()) {
-//       $response = ['status' => 1, 'message' => 'Record created successfully.'];
-//     } else {
-//       $response = ['status' => 0, 'message' => 'Failed to create record.'];
-//     }
-//     echo json_encode($response);
-//     break;
-// }
+    if ($stmt->execute()) {
+      $response = ['status' => 1, 'message' => 'Record created successfully.'];
+    } else {
+      $response = ['status' => 0, 'message' => 'Failed to create record.'];
+    }
+    echo json_encode($response);
+    break;
+}
 
 /**
  * Function to check both internet and database connectivity
